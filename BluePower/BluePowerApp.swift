5436ba59506f4d5ae4a452aa985691addbd859f1
JunYeong-J <eeot2827@gmail.com>
2025-04-14 14:49:11 +0900

    init: 초기설정

---
+++ BluePower/BluePowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluePowerApp.swift
+//  BluePower
+//
+//  Created by 전준영 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BluePowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
