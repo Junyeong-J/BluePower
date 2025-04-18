ec85b15bb69ce1a6ee5e3c07e093899b157bdd1b
JunYeong-J <eeot2827@gmail.com>
2025-04-18 17:24:01 +0900

    feat: 메인뷰에 테이블 추가

--- BluePower/Present/MainView.swift
+++ BluePower/Present/MainView.swift
@@ -41,6 +41,9 @@ struct MainView: View {
                     }
                     .padding(.horizontal, 20)
                     
+                    BluetoothDeviceListView(viewModel: viewModel)
+                        .transition(.opacity)
+                    
                     Spacer()
                 }
             }
